c0b71e4623d0f53aa730a95ae8b0eaf43b6d5b81
Tabaei, Seyedmasih <seyedmasih.tabaei@stud.hs-coburg.de>
2024-12-07 17:27:49 +0100

    Starts #9 and contributes to #1 and #3
    Duplicated the project created for naively rendering a triangle with a signle
    thread to create a new version this time with multiple threads,
    integrated the new project both into the build system and the Git(Lab),
    renamed both projects and also fixed the shader paths in the older
    project

---
+++ Tutorials/TXNaiveTriangleWithMeshShaderWithMultipleThreads/shaders/NaiveTriangle.hlsl
@@ -0,0 +1,46 @@
+#define NUM_THREADS_X 1
+#define NUM_THREADS_Y 1
+#define NUM_THREADS_Z 1
+
+#define MAX_NUM_VERTICES 3
+#define MAX_NUM_TRIANGLES 1
+
+static const float3 userDefinedVertices[] = { { 0.0f, 0.25f, 0.0f }, { 0.25f, -0.25f, 0.0f }, { -0.25f, -0.25f, 0.0f } };
+static const float3 userDefinedColor = float3(1.0f, 1.0f, 0.0f);
+
+struct MeshShaderOutput
+{
+  float4 position : SV_POSITION;
+};
+
+[outputtopology("triangle")]
+[numthreads(NUM_THREADS_X, NUM_THREADS_Y, NUM_THREADS_Z)]
+void MS_main(
+    out vertices MeshShaderOutput triangleVertices[MAX_NUM_VERTICES],
+    out indices uint3 triangleIndices[MAX_NUM_TRIANGLES]
+)
+{
+    SetMeshOutputCounts(MAX_NUM_VERTICES, MAX_NUM_TRIANGLES);
+    
+    MeshShaderOutput firstVertex;
+    firstVertex.position = float4(userDefinedVertices[0].x, userDefinedVertices[0].y, userDefinedVertices[0].z, 1.0f);
+    
+    MeshShaderOutput secondVertex;
+    secondVertex.position = float4(userDefinedVertices[1].x, userDefinedVertices[1].y, userDefinedVertices[1].z, 1.0f);
+    
+    MeshShaderOutput thirdVertex;
+    thirdVertex.position = float4(userDefinedVertices[2].x, userDefinedVertices[2].y, userDefinedVertices[2].z, 1.0f);
+    
+    triangleVertices[0] = firstVertex;
+    triangleVertices[1] = secondVertex;
+    triangleVertices[2] = thirdVertex;
+    
+    triangleIndices[0] = uint3(0, 1, 2);
+
+}
+
+float4 PS_main(MeshShaderOutput input)
+    : SV_TARGET
+{
+    return float4(userDefinedColor, 1.0f);
+}
