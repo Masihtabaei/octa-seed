cbfa5719e280c4f103c160a4080e4ecaccf862e3
Tabaei, Seyedmasih <seyedmasih.tabaei@stud.hs-coburg.de>
2025-01-24 10:05:52 +0100

    Starts the work on #21
    Finished an initial implementation for the apple

---
+++ Assignments/AXSimpleAppleWithIntraLOD/shaders/Apple.hlsl
@@ -0,0 +1,124 @@
+#define NUM_THREADS_X 11
+#define NUM_THREADS_Y 11
+#define NUM_THREADS_Z 1
+
+static const float3 userDefinedColor = float3(1.0f, 0.0f, 0.0f);
+
+static const float3 p0 = float3(0.0f, 0.0f, -0.094f);
+static const float3 p1 = float3(1.0f, 0.0f, -0.7f);
+static const float3 p2 = float3(1.0f, 0.0f, 0.943f);
+static const float3 p3 = float3(0.f, 0.0f, 1.0f);
+
+cbuffer PerMeshConstants : register(b0)
+{
+    float4x4 transformationMatrix;
+    int intraLOD;
+}
+
+struct MeshShaderOutput
+{
+    float4 position : SV_POSITION;
+};
+
+float map(float value, float min1, float max1, float min2, float max2)
+{
+    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
+float signNotZero(float k)
+{
+    return (k >= 0.0) ? 1.0 : -1.0;
+}
+
+
+float2 signNotZero(float2 v)
+{
+    return float2(signNotZero(v.x), signNotZero(v.y));
+}
+
+float3 octDecode(float2 o)
+{
+    float3 v = float3(o.x, o.y, 1.0 - abs(o.x) - abs(o.y));
+    if (v.z < 0.0f)
+    {
+        v.xy = (1.0 - abs(v.yx)) * signNotZero(v.xy);
+    }
+    return normalize(v);
+}
+
+float3 evaluateQuadraticBezier(float t)
+{
+    float3 firstLerp = lerp(p0.xyz, p1.xyz, t);
+    float3 secondLerp = lerp(p2.xyz, p3.xyz, t);
+    float3 result = lerp(firstLerp, secondLerp, t);
+
+    return result;
+}
+
+float3 evaluateCubicBezierCurve(float t)
+{
+    float3 firstLerp = lerp(p0.xyz, p1.xyz, t);
+    float3 secondLerp = lerp(p1.xyz, p2.xyz, t);
+    float3 thirdLerp = lerp(p2.xyz, p3.xyz, t);
+    float3 fourthLerp = lerp(firstLerp, secondLerp, t);
+    float3 fifthLerp = lerp(secondLerp, thirdLerp, t);
+    float3 result = lerp(fourthLerp, fifthLerp, t);
+
+    return result;
+}
+
+[outputtopology("triangle")]
+[numthreads(NUM_THREADS_X, NUM_THREADS_Y, NUM_THREADS_Z)]
+void MS_main(
+    in uint3 threadIdInsideItsGroup : SV_GroupThreadID,
+    out vertices MeshShaderOutput triangleVertices[NUM_THREADS_X * NUM_THREADS_Y],
+    out indices uint3 triangleIndices[2 * (NUM_THREADS_X - 1) * (NUM_THREADS_Y - 1)]
+)
+{
+    SetMeshOutputCounts(intraLOD * intraLOD, 2 * (intraLOD - 1) * (intraLOD - 1));
+    if (threadIdInsideItsGroup.x < intraLOD && threadIdInsideItsGroup.y < intraLOD)
+    {
+        float xCoordinate = map(threadIdInsideItsGroup.x, 0.0f, intraLOD - 1, -1.0f, 1.0f);
+        float yCoordinate = map(threadIdInsideItsGroup.y, 0.0f, intraLOD - 1, -1.0f, 1.0f);
+        float3 decodedCoordinates = octDecode(float2(xCoordinate, yCoordinate));
+
+        float t = (decodedCoordinates.z + 1.0f) / 2;
+        float3 evaluatedCoordinates = evaluateCubicBezierCurve(t);
+        float2 sc = float2(0.0f, 0.0f);
+        
+        //sc = normalize(decodedCoordinates.yx);
+        //evaluatedCoordinates.xy = mul(float2x2(sc.y, -sc.x, sc.x, sc.y), evaluatedCoordinates.xy);
+   
+        float2 angle = atan2(decodedCoordinates.y, decodedCoordinates.x);
+        evaluatedCoordinates.xy = mul(float2x2(cos(angle.x), -sin(angle.x), sin(angle.x), cos(angle.x)), evaluatedCoordinates.xy);
+        
+        triangleVertices[threadIdInsideItsGroup.y * intraLOD + threadIdInsideItsGroup.x].position = mul(transformationMatrix, float4(evaluatedCoordinates, 1.0f));
+
+        if ((threadIdInsideItsGroup.x < (intraLOD - 1)) && (threadIdInsideItsGroup.y < (intraLOD - 1)))
+        {
+            uint currentVertexID = threadIdInsideItsGroup.y * intraLOD + threadIdInsideItsGroup.x;
+            uint nextMostRightVertexID = currentVertexID + 1;
+            uint nextMostBottomVertexID = currentVertexID + intraLOD;
+            uint nextMostBottomRightVertexID = nextMostBottomVertexID + 1;
+            if ((threadIdInsideItsGroup.x < (intraLOD / 2) && threadIdInsideItsGroup.y < (intraLOD / 2)) || (threadIdInsideItsGroup.x >= (intraLOD / 2)
+             && threadIdInsideItsGroup.y >= (intraLOD / 2)))
+            {
+                triangleIndices[2 * (threadIdInsideItsGroup.y * (intraLOD - 1) + threadIdInsideItsGroup.x)] = uint3(currentVertexID, nextMostRightVertexID, nextMostBottomVertexID).xyz;
+                triangleIndices[2 * (threadIdInsideItsGroup.y * (intraLOD - 1) + threadIdInsideItsGroup.x) + 1] = uint3(nextMostRightVertexID, nextMostBottomRightVertexID, nextMostBottomVertexID).xyz;
+
+            }
+            else
+            {
+                triangleIndices[2 * (threadIdInsideItsGroup.y * (intraLOD - 1) + threadIdInsideItsGroup.x)] = uint3(currentVertexID, nextMostRightVertexID, nextMostBottomRightVertexID).xyz;
+                triangleIndices[2 * (threadIdInsideItsGroup.y * (intraLOD - 1) + threadIdInsideItsGroup.x) + 1] = uint3(nextMostBottomRightVertexID, nextMostBottomVertexID, currentVertexID).xyz;
+            }
+        }
+    }
+
+}
+
+float4 PS_main(MeshShaderOutput input)
+    : SV_TARGET
+{
+    return float4(userDefinedColor, 1.0f);
+}
